97cb0199fa7e3d462b53ac7817290acb37fd781c
Sergey Zakhlystun <zakhlystun@gmail.com>
2024-03-22 20:29:20 +0300

    Введены параметры из задания

--- src/test/java/ru/netology/webselenium/AppOrderNegativeTest.java
+++ src/test/java/ru/netology/webselenium/AppOrderNegativeTest.java
@@ -1,12 +1,12 @@
 package ru.netology.webselenium;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -14,13 +14,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppOrderNegativeTest {
-    private ChromeDriver driver;
-    //private WebDriver driver;
+    private WebDriver driver;
 
     @BeforeAll
     public static void setupAll() {
-
-        //WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -31,6 +29,7 @@ public class AppOrderNegativeTest {
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
+
     }
 
     @AfterEach
